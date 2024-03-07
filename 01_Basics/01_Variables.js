1b7ba81e8ef8a04e46235542844b24c5ff0bc69a
ArjunAhir1718 <137472616+ArjunAhir1718@users.noreply.github.com>
2024-03-07 06:20:55 +0000

    Datatype

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,18 @@
+const accontId = 14435          
+let email = "arjun1234@gmail.com"
+var accountPass = "453533"
+accountCity = "Jaipur"
+let accountState;
+
+email = "Ak12@gmail.com"
+accountPass = 2121212
+accountCity =  "Bangaluru"
+
+
+// Not Prefer to use var 
+
+// accontId = 24434  //Const Did't Change
+
+console.log(accontId);
+
+console.table([accontId, email, accountPass, accountCity, accountState])
---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,27 @@
+"use strict";  //Treat all js code in new version
+
+// alert(3 + 3)  //we are using node.js ,  not brower 
+
+console.log(3 + 3); 
+
+console.log("Arjun");
+
+let name = "Arjun"
+let age = 19
+let isLogIn = true
+let state;
+
+//premetive Datatype
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// Boolean => true/false
+// null => standalone value 
+// undefined => No Defined 
+// symbol => unique
+
+//object
+
+console.log(typeof undefined);  // undefined 
+console.log(typeof null);  // Object
