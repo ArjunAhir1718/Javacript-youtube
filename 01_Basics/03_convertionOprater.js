dd20fd8dd9c4321ee0450fd18dac92e17f47fc17
ArjunAhir1718 <137472616+ArjunAhir1718@users.noreply.github.com>
2024-03-07 14:26:16 +0000

    Convertion of DataType

---
+++ 01_Basics/03_convertionOprater.js
@@ -0,0 +1,38 @@
+//COnvert in Number()
+let score = undefined
+
+// console.log(typeof score);
+// console.log(typeof (score));
+ 
+let valueInNumber = Number(score)  // Convert in Number to use Number()
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+//Convert in number differnce things 
+// "33" => 33
+// "33abd" => NaN
+// true => 1 , false => 0
+// undefined = NaN
+// null = 0
+
+
+//Convert in Boolean()
+let userLoggedIn = "Arjun"
+
+let logedInBoolean = Boolean(userLoggedIn)
+// console.log(logedInBoolean);
+
+
+// convert in Boolean()
+// 1 => true , 0 => flase
+// "" => false
+// "Arjun" => true
+
+// convert in String
+
+let some = 33
+
+let convertString = String(some)
+console.log(convertString);
+console.log(typeof convertString);
